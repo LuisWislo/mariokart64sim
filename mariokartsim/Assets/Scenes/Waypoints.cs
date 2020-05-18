f323e1e5151eb3f14b6919e656450ffe80b8c811
Charlie Lopez <a01633683@itesm.mx>
2020-05-17 23:42:33 -0500

    Add movement sistem for simulation

---
+++ mariokartsim/Assets/Scenes/Waypoints.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    // put the points from unity interface
+    public Transform[] wayPointList;
+
+    public int currentWayPoint = 0;
+    public int currentLap = 1;
+    public int laps = 3;
+    Transform targetWayPoint;
+
+    public float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if we have somewere to walk
+        if (currentLap <= laps)
+        {
+            if (targetWayPoint == null)
+                targetWayPoint = wayPointList[currentWayPoint];
+            walk();
+        }
+    }
+
+    void walk()
+    {
+        // rotate towards the target
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+
+        // move towards the target
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+        if (transform.position == targetWayPoint.position)
+        {
+            if (currentWayPoint == 22)
+            {
+                currentLap++;
+            }
+            currentWayPoint = (currentWayPoint + 1) % 23;
+            targetWayPoint = wayPointList[currentWayPoint];
+        }
+    }
+}
