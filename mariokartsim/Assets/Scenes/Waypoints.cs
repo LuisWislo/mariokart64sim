ffbdfd6bdbb0f61b1e8cb346798857584e4f6b76
Charlie Lopez <a01633683@itesm.mx>
2020-05-20 18:19:12 -0500

    Add kart simulation (again)
    Added random shortcut transit.
    Has a bug tho.

--- mariokartsim/Assets/Scenes/Waypoints.cs
+++ mariokartsim/Assets/Scenes/Waypoints.cs
@@ -1,54 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = System.Random;
 
 public class Waypoints : MonoBehaviour
 {
+    private int currentWayPoint = 1; //Sigue indices de 0 a n-1
+    private int currentLap = 1;
+    private int laps = 3;
+    private float speed = 20f;
 
-    // put the points from unity interface
-    public Transform[] wayPointList;
-
-    public int currentWayPoint = 0;
-    public int currentLap = 1;
-    public int laps = 3;
+    private int availableWaypoints = 25;
     Transform targetWayPoint;
+    private Transform[] wayPointList; //Sigue indices de 0 a n-1
+    private int[] initShorcut = {3,10,13}; //Waypoints con acceso a shortcuts
+    private int[] endShortcut = {4,13,18}; //Waypoints terminales de shortcuts
 
-    public float speed = 10f;
+    private Boolean throughShortcut = false;
+    //private Boolean isRacing = false;
 
-    // Use this for initialization
     void Start()
     {
-
+        wayPointList = new Transform[availableWaypoints];
+        for (int wp = 0; wp < wayPointList.Length; wp++)
+        {
+            wayPointList[wp] = GameObject.Find("Waypoint" + (wp)).transform;
+        }
+        targetWayPoint = wayPointList[currentWayPoint];
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // check if we have somewere to walk
         if (currentLap <= laps)
         {
-            if (targetWayPoint == null)
-                targetWayPoint = wayPointList[currentWayPoint];
             walk();
+            if (transform.position == targetWayPoint.position)
+            {
+                updateWaypoint();
+            }
         }
     }
 
     void walk()
     {
-        // rotate towards the target
         transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
-
-        // move towards the target
         transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+    }
 
-        if (transform.position == targetWayPoint.position)
+    void updateWaypoint()
+    {
+        if (currentWayPoint == 0)
         {
-            if (currentWayPoint == 22)
+            currentLap++;
+        }
+
+        currentWayPoint = (currentWayPoint + 1) % (availableWaypoints);
+
+        GameObject possibleShortcut = GameObject.Find("Waypoints" + (currentWayPoint));
+        if (!this.throughShortcut)
+        {
+            if (initShorcut.Contains(currentWayPoint-1) && possibleShortcut != null && UnityEngine.Random.value < 0.5)
             {
-                currentLap++;
+                this.throughShortcut = true;
+                targetWayPoint = possibleShortcut.transform;
             }
-            currentWayPoint = (currentWayPoint + 1) % 23;
-            targetWayPoint = wayPointList[currentWayPoint];
+            else
+            {
+                targetWayPoint = wayPointList[currentWayPoint];
+            }
+        }
+        else
+        {
+            if (possibleShortcut != null)
+            {
+                targetWayPoint = possibleShortcut.transform;
+            }
+            else
+            {
+                this.throughShortcut = false;
+                targetWayPoint = wayPointList[currentWayPoint];
+            }
         }
     }
 }
