b93b143d42fffcc395af2dbfacb2057d620e9896
LuisWislo <luisfwilson1998@gmail.com>
2020-05-21 04:19:12 -0500

    get items

---
+++ mariokartsim/Assets/Scripts/Kart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kart : MonoBehaviour
+{
+    public Transform kart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "item")
+        {
+            GetItem();
+        }
+    }
+
+    void GetItem()
+    {
+        Debug.Log("Got item: " + ItemBox.GetItem(2));
+    }
+}
