6842dfbe115f73e3190731130f9ed8c222ea3b2e
LuisWislo <luisfwilson1998@gmail.com>
2020-05-24 06:14:54 -0500

    character & item prefabs

---
+++ mariokartsim/Assets/Scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] currentPlaces;
+    private float[][] targetPositions;
+
+    void Start()
+    {
+        /*this.currentPlaces = null;
+        this.targetPositions = new float[][]
+        {
+
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void receivePlaces(GameObject[] places)
+    {
+        this.currentPlaces = places;
+    }
+
+    private void changePlaces()
+    {
+
+    }
+
+
+}
