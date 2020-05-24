e3bbe34f20407226ca5299faaae220bf71020b2b
Charlie Lopez <a01633683@itesm.mx>
2020-05-24 12:42:13 -0500

    Add position place manager
    +Added script to manage kart positions
    +Added possible changes to Waypoints management scripts

--- mariokartsim/Assets/Scenes/Waypoints.cs
+++ mariokartsim/Assets/Scenes/Waypoints.cs
@@ -10,13 +10,17 @@ public class Waypoints : MonoBehaviour
     private int currentWayPoint = 1; //Sigue indices de 0 a n-1
     private int currentLap = 1;
     private int laps = 3;
-    private float speed = 20f;
+    private float speed = 50f;
 
     private int availableWaypoints = 25;
     Transform targetWayPoint;
     private Transform[] wayPointList; //Sigue indices de 0 a n-1
-    private int[] initShorcut = {3,10,13}; //Waypoints con acceso a shortcuts
-    private int[] endShortcut = {4,13,18}; //Waypoints terminales de shortcuts
+    private int[] sShortcutAvailable = {11,12,14,15,16,17};
+    private int[] mShortcutAvailable = {3,10,13};
+    private Boolean canShortcut = false;
+    //private int[] shortcut1 = {4};
+    //private int[] shortcut2 = {11, 12, 13};
+    //private int[] shortcut3 = {14, 15, 16, 17, 18};
 
     private Boolean throughShortcut = false;
     //private Boolean isRacing = false;
@@ -28,6 +32,28 @@ public class Waypoints : MonoBehaviour
         {
             wayPointList[wp] = GameObject.Find("Waypoint" + (wp)).transform;
         }
+
+        /*
+        for (int wp = 0; wp < wayPointList.Length; wp++)
+        {
+            int count = 0;
+            if (GameObject.Find("Waypoints" + (wp)) != null)
+            {
+                for (int sc = 0; sc < wayPointList.Length; sc++)
+                {
+                    GameObject possibleShortcut = GameObject.Find("Waypoints" + (sc + wp));
+                    if (possibleShortcut != null)
+                    {
+                        shortcuts[count][sc] = possibleShortcut.transform;
+                    }
+                    else
+                    {
+                        break;
+                    }
+            }
+        }
+        */
+
         targetWayPoint = wayPointList[currentWayPoint];
     }
 
@@ -57,11 +83,10 @@ public class Waypoints : MonoBehaviour
         }
 
         currentWayPoint = (currentWayPoint + 1) % (availableWaypoints);
-
         GameObject possibleShortcut = GameObject.Find("Waypoints" + (currentWayPoint));
-        if (!this.throughShortcut)
+        if (!this.throughShortcut && this.mShortcutAvailable.Contains(currentWayPoint))
         {
-            if (initShorcut.Contains(currentWayPoint-1) && possibleShortcut != null && UnityEngine.Random.value < 0.5)
+            if (possibleShortcut != null && UnityEngine.Random.value < 0.5)
             {
                 this.throughShortcut = true;
                 targetWayPoint = possibleShortcut.transform;
@@ -71,17 +96,14 @@ public class Waypoints : MonoBehaviour
                 targetWayPoint = wayPointList[currentWayPoint];
             }
         }
+        else if (this.throughShortcut && this.sShortcutAvailable.Contains(currentWayPoint))
+        {
+            targetWayPoint = possibleShortcut.transform;
+        }
         else
         {
-            if (possibleShortcut != null)
-            {
-                targetWayPoint = possibleShortcut.transform;
-            }
-            else
-            {
-                this.throughShortcut = false;
-                targetWayPoint = wayPointList[currentWayPoint];
-            }
+            this.throughShortcut = false;
+            targetWayPoint = wayPointList[currentWayPoint];
         }
     }
 }
---
+++ mariokartsim/Assets/Scripts/HMMSync.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class HMMSync
+{
+    private int usedPlaces = 0;
+    private static float[][] placeProbabilities = new float[][]
+    {
+        //           1st    2nd    3rd    4th    5th    6th    7th    8th
+        new float[] {0.45f, 0.30f, 0.10f, 0.10f, 0.05f, 0.00f, 0.00f, 0.00f}, //1st
+        new float[] {0.30f, 0.40f, 0.15f, 0.05f, 0.05f, 0.05f, 0.00f, 0.00f}, //2nd
+        new float[] {0.15f, 0.35f, 0.25f, 0.15f, 0.10f, 0.00f, 0.00f, 0.00f}, //3rd
+        new float[] {0.10f, 0.20f, 0.30f, 0.10f, 0.15f, 0.10f, 0.05f, 0.00f}, //4th
+        new float[] {0.00f, 0.10f, 0.15f, 0.25f, 0.10f, 0.20f, 0.10f, 0.10f}, //5th
+        new float[] {0.00f, 0.05f, 0.15f, 0.25f, 0.25f, 0.05f, 0.15f, 0.10f}, //6th
+        new float[] {0.00f, 0.05f, 0.20f, 0.40f, 0.15f, 0.10f, 0.05f, 0.05f}, //7th
+        new float[] {0.00f, 0.00f, 0.05f, 0.35f, 0.35f, 0.15f, 0.05f, 0.05f}  //8th
+    };
+
+    private float[][] syncedPlaces = new float[][]
+    {
+        //           1st    2nd    3rd    4th    5th    6th    7th    8th
+        new float[] {0.45f, 0.30f, 0.10f, 0.10f, 0.05f, 0.00f, 0.00f, 0.00f}, //1st
+        new float[] {0.30f, 0.40f, 0.15f, 0.05f, 0.05f, 0.05f, 0.00f, 0.00f}, //2nd
+        new float[] {0.15f, 0.35f, 0.25f, 0.15f, 0.10f, 0.00f, 0.00f, 0.00f}, //3rd
+        new float[] {0.10f, 0.20f, 0.30f, 0.10f, 0.15f, 0.10f, 0.05f, 0.00f}, //4th
+        new float[] {0.00f, 0.10f, 0.15f, 0.25f, 0.10f, 0.20f, 0.10f, 0.10f}, //5th
+        new float[] {0.00f, 0.05f, 0.15f, 0.25f, 0.25f, 0.05f, 0.15f, 0.10f}, //6th
+        new float[] {0.00f, 0.05f, 0.20f, 0.40f, 0.15f, 0.10f, 0.05f, 0.05f}, //7th
+        new float[] {0.00f, 0.00f, 0.05f, 0.35f, 0.35f, 0.15f, 0.05f, 0.05f}  //8th
+    };
+
+    private void RestartArray()
+    {
+        Array.Copy(placeProbabilities, this.syncedPlaces, placeProbabilities[0].Length);
+    }
+
+    public int GetPlace(int place)
+    {
+        float random = Random.Range(0.0f, 1.0f);
+        int newPlace = -1;
+        while(newPlace == -1) {
+            float rng = (float)System.Math.Round(random * 100f) / 100f;
+            float portion = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                portion += placeProbabilities[place][i];
+                if (rng <= portion)
+                {
+                    newPlace = i;
+                    this.usedPlaces++;
+                    if (this.usedPlaces < placeProbabilities[0].Length)
+                    {
+                        this.SyncPlaces(i);
+                    }
+                    else
+                    {
+                        this.RestartArray();
+                    }
+                }
+            }
+        }
+        return newPlace;
+    }
+
+    private void SyncPlaces(int usedPlace)
+    {
+        for (int placeArray = 0; placeArray < placeProbabilities[0].Length; placeArray++)
+        {
+            float currentPlaceProb = this.syncedPlaces[placeArray][usedPlace];
+            this.syncedPlaces[placeArray][usedPlace] = 0.0f;
+            for (int place = 0; place < this.syncedPlaces[placeArray].Length; place++)
+            {
+                if(this.syncedPlaces[placeArray][place] != 0.0f) {
+                    this.syncedPlaces[placeArray][place] += currentPlaceProb / (placeProbabilities[0].Length - this.usedPlaces);
+                }
+            }
+        }
+    }
+}
