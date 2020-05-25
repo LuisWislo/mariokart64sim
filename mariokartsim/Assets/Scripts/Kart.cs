4afa4066e46e495bc4e9e5af40b9208918de9d84
Charlie Lopez <a01633683@itesm.mx>
2020-05-25 00:47:19 -0500

    Adapt scene

--- mariokartsim/Assets/Scripts/Kart.cs
+++ mariokartsim/Assets/Scripts/Kart.cs
@@ -13,42 +13,53 @@ public class Kart : MonoBehaviour
     private int place;
     private string item;
     public Transform kart;
+    private Transform kartManager;
     private GameObject syncManager;
     private HMMSync syncer;
+    
+    private Transform[] innerWP;
+    private Transform targetIWP;
+    private float speed = 2f;
+    private int currentIWP;
+    private int availableIWP = 8;
 
-    private int currentWayPoint = 1; //Sigue indices de 0 a n-1
-    private int currentLap = 1;
-    private int laps = 3;
-    private float speed = 15f;
     private int availableWaypoints = 25;
     Transform targetWayPoint;
     private Transform[] wayPointList; //Sigue indices de 0 a n-1
+    private int currentWayPoint = 1; //Sigue indices de 0 a n-1
 
     void Start()
     {
-        this.syncManager = GameObject.Find("SyncManager");
-        this.syncer = this.syncManager.GetComponent<HMMSync>();
-        this.place = Int32.Parse(this.name.Substring(4)) - 1;
-        this.item = "";
-        GetInfo();
+        this.kartManager = GameObject.Find("KartManager").transform;
+        innerWP = new Transform[8];
+        for (int wp = 0; wp < innerWP.Length; wp++)
+        {
+            innerWP[wp] = GameObject.Find("iwp" + wp).transform;
+            innerWP[wp].GetComponent<MeshRenderer>().enabled = false;
+        }
 
         wayPointList = new Transform[availableWaypoints];
         for (int wp = 0; wp < wayPointList.Length; wp++)
         {
             wayPointList[wp] = GameObject.Find("Waypoint" + (wp)).transform;
+            //wayPointList[wp].GetComponent<MeshRenderer>().enabled = false;
         }
         targetWayPoint = wayPointList[currentWayPoint];
+
+        this.syncManager = GameObject.Find("SyncManager");
+        this.syncer = this.syncManager.GetComponent<HMMSync>();
+        this.place = this.currentIWP = (int)Int64.Parse(this.name.Substring(4)) - 1;
+        this.item = "";
+        //GetInfo();
+        targetIWP = innerWP[(this.currentIWP + 1) % 8];
     }
     
     void Update()
     {
-        if (currentLap <= laps)
+        Walk();
+        if (this.kartManager.position == targetWayPoint.position)
         {
-            Walk();
-            if (transform.position == targetWayPoint.position)
-            {
-                UpdateWaypoint();
-            }
+            UpdateWaypoint();
         }
     }
 
@@ -59,7 +70,7 @@ public class Kart : MonoBehaviour
             //Asignar valores a GUI
             print("Got place: " + places[this.place]);
             print("Got item: " + this.item);
-            GetInfo();
+            //GetInfo();
         }
     }
 
@@ -68,21 +79,16 @@ public class Kart : MonoBehaviour
         this.place = this.syncer.GetPlace(this.place);
         this.item = ItemBox.getItem(this.place);
     }
-    
+
     void Walk()
     {
-        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
-        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+        transform.forward = Vector3.RotateTowards(transform.forward, targetIWP.position - transform.position, speed * Time.deltaTime, 0.0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetIWP.position, speed * Time.deltaTime);
     }
-    
+
     void UpdateWaypoint()
     {
-        if (currentWayPoint == 0)
-        {
-            currentLap++;
-        }
-
-        currentWayPoint = (currentWayPoint + 1) % (availableWaypoints);
-        targetWayPoint = wayPointList[currentWayPoint];
+        currentIWP = (currentIWP + 1) % (availableIWP);
+        targetIWP = innerWP[currentIWP];
     }
 }
---
+++ mariokartsim/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    private int currentWayPoint = 1; //Sigue indices de 0 a n-1
+    private int currentLap = 1;
+    private int laps = 3;
+    private float speed = 5f;
+    private int availableWaypoints = 25;
+    Transform targetWayPoint;
+    private Transform[] wayPointList; //Sigue indices de 0 a n-1
+
+    void Start()
+    {
+        wayPointList = new Transform[availableWaypoints];
+        for (int wp = 0; wp < wayPointList.Length; wp++)
+        {
+            wayPointList[wp] = GameObject.Find("Waypoint" + (wp)).transform;
+        }
+        targetWayPoint = wayPointList[currentWayPoint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentLap <= laps)
+        {
+            Walk();
+            if (transform.position == targetWayPoint.position)
+            {
+                UpdateWaypoint();
+            }
+        }
+    }
+
+    void Walk()
+    {
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWayPoint.position - transform.position, speed * Time.deltaTime, 0.0f);
+        transform.position = Vector3.MoveTowards(transform.position, targetWayPoint.position, speed * Time.deltaTime);
+    }
+
+    void UpdateWaypoint()
+    {
+        if (currentWayPoint == 0)
+        {
+            currentLap++;
+        }
+
+        currentWayPoint = (currentWayPoint + 1) % (availableWaypoints);
+        targetWayPoint = wayPointList[currentWayPoint];
+    }
+}
